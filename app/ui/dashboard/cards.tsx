d7e0da5ebede82f7dd5e8381a30aa8791af00c97
Vasavi K <vasavikasireddi@gmail.com>
2024-10-17 16:50:28 +0530

    Upto Streaming revenue chart

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -18,6 +18,7 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
+
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -26,6 +27,7 @@ export default async function CardWrapper() {
         value={numberOfCustomers}
         type="customers"
       /> */}
+
     </>
   );
 }
